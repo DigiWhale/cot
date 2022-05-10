f22fdb1826c33a55e9c2c0202d87561d2e726ea5
digiwhale <digiwhale.humphrey@gmail.com>
2022-05-09 23:01:27 -0400

    updated to python3

--- testCoT.py
+++ testCoT.py
@@ -2,13 +2,13 @@ import os
 import time
 import CoT
 
-ATAK_IP = os.getenv('ATAK_IP', '192.168.1.160')
+ATAK_IP = os.getenv('ATAK_IP', '10.0.0.104')
 ATAK_PORT = int(os.getenv('ATAK_PORT', '4242'))
 ATAK_PROTO = os.getenv('ATAK_PROTO', 'UDP')
 
 params = {  # SWX parking lot
-    "lat": 27.957261,
-    "lon": -82.436587,
+    "lat": 38.8892687,
+    "lon": -77.0518607,
     "uid": "Nerd Herd",
     "identity": "hostile",
     "dimension": "land-unit",
@@ -18,20 +18,19 @@ params = {  # SWX parking lot
 }
 
 for i in range(0, 10):
-    params["lat"] = params["lat"] + i/10000.0
-    params["lon"] = params["lon"] + i/10000.0
-    print "Params:\n" + str(params)
+    #    params["lat"] = params["lat"] + i/10000.0
+    #    params["lon"] = params["lon"] + i/10000.0
+    print("Params:\n" + str(params))
     cot = CoT.CursorOnTarget()
     cot_xml = cot.atoms(params)
 
+    print("\nXML message:")
+    print(cot_xml)
 
-    print "\nXML message:"
-    print cot_xml
-    
-    print "\nPushing to ATAK..."
+    print("\nPushing to ATAK...")
     if ATAK_PROTO == "TCP":
-      sent = cot.pushTCP(ATAK_IP, ATAK_PORT, cot_xml)
-    else
-      sent = cot.pushUDP(ATAK_IP, ATAK_PORT, cot_xml)
-    print str(sent) + " bytes sent to " + ATAK_IP + " on port " + str(ATAK_PORT)
+        sent = cot.pushTCP(ATAK_IP, ATAK_PORT, cot_xml)
+    else:
+        sent = cot.pushUDP(ATAK_IP, ATAK_PORT, cot_xml)
+    print(str(sent) + " bytes sent to " + ATAK_IP + " on port " + str(ATAK_PORT))
     time.sleep(2)
